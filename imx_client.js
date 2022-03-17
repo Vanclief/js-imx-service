9b44a445103221b996269e8b923525f38e51d83d
Franco Valencia <franco.avalencia@gmail.com>
2022-03-17 10:11:36 -0600

    First basic implementation

---
+++ imx_client.js
@@ -0,0 +1,114 @@
+/** @format */
+
+import {
+  ImmutableXClient,
+  ETHTokenType,
+  ERC721TokenType,
+  ERC20TokenType,
+} from "@imtbl/imx-sdk";
+
+import { BigNumber } from "ethers";
+import { Wallet } from "@ethersproject/wallet";
+import { AlchemyProvider } from "@ethersproject/providers";
+
+const networkParams = {
+  ropsten: {
+    apiUrl: "https://api.ropsten.x.immutable.com/v1",
+    starkContractAddr: "0x4527BE8f31E2ebFbEF4fCADDb5a17447B27d2aef",
+    registratrionContractAddr: "0x6C21EC8DE44AE44D0992ec3e2d9f1aBb6207D864",
+  },
+  mainnet: {
+    apiUrl: "https://api.x.immutable.com/v1",
+    starkContractAddr: "0x5FDCCA53617f4d2b9134B29090C87D01058e27e9",
+    registratrionContractAddr: "0x72a06bf2a1CE5e39cBA06c0CAb824960B587d64c",
+  },
+};
+
+export class IMXClient {
+  async init(request) {
+    // Get the network configuration
+    let selectedNetwork;
+
+    if (request.network === "ropsten") selectedNetwork = networkParams.ropsten;
+    else if (request.network === "mainet")
+      selectedNetwork = networkParams.mainnet;
+    else throw Error(`[TYPESCRIPTWRAPPER]: Unknown network type: '${network}'`);
+
+    // Create the signer
+    const provider = new AlchemyProvider(request.network, request.alchemy_key);
+    const signer = new Wallet(request.private_key).connect(provider);
+
+    this.publicKey = signer.address;
+
+    // Create the client
+    this.client = await ImmutableXClient.build({
+      publicApiUrl: selectedNetwork.apiUrl,
+      signer: signer,
+      starkContractAddress: selectedNetwork.starkContractAddr,
+      registrationContractAddress: selectedNetwork.registratrionContractAddr,
+      // gasLimit: gasLimit,
+      // gasPrice: gasPrice,
+    });
+
+    return this.client;
+  }
+
+  isInstatiated() {
+    if (this.client === undefined) {
+      throw Error("IMX client not instantiated");
+    }
+  }
+
+  async register() {
+    this.isInstatiated();
+
+    return await this.client.registerImx({
+      etherKey: this.client.address.toLowerCase(),
+      starkPublicKey: this.client.starkPublicKey,
+    });
+  }
+
+  async transferERC20(request) {
+    // request.sender = this.publicKey;
+    request.quantity = BigNumber.from(request.quantity);
+
+    return await this.client.transferV2F(request);
+  }
+
+  async transferERC721(request) {
+    console.log("transferERC721", request);
+
+    const params = {
+      sender_ether_key: this.publicKey,
+      transfer_request: [
+        {
+          token: {
+            type: "ERC721",
+            data: {
+              tokenId: request.token_id,
+              tokenAddress: request.token_address,
+            },
+          },
+          amount: BigNumber.from(1),
+          receiver: request.to_address,
+        },
+      ],
+    };
+
+    console.log("Params", params);
+
+    return await this.client.transferV2(params);
+  }
+
+  async approveERC20(request) {
+    request.sender = this.publicKey;
+
+    return await this.client.approveERC20(request);
+  }
+
+  async approveERC721(request) {
+    request.sender = this.publicKey;
+
+    return await this.client.approveNFT(request);
+  }
+}
---
+++ server.js
@@ -0,0 +1,107 @@
+/** @format */
+
+// Require the framework and instantiate it
+import Fastify from "fastify";
+import { IMXClient } from "./imx_client.js";
+// import { ImmutableXClient, ETHTokenType, ERC721TokenType, ERC20TokenType } from '@imtbl/imx-sdk'
+
+// const fastify = require("fastify")({ logger: true });
+
+const server = Fastify({ logger: true });
+const client = new IMXClient();
+
+server.post("/init", async (request, reply) => {
+  try {
+    const response = await client.init(request.body);
+    reply
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send({ payload: response });
+  } catch (err) {
+    reply
+      .code(400)
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send({ err });
+  }
+});
+
+server.get("/register", async (request, reply) => {
+  try {
+    const response = await client.register();
+    reply
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send({ payload: response });
+  } catch (err) {
+    reply = err;
+  }
+
+  return reply;
+});
+
+server.post("/erc20/transfer", async (request, reply) => {
+  try {
+    const response = await client.transferER20(request.body);
+    reply
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send({ payload: response });
+  } catch (err) {
+    console.log("err", err);
+    reply
+      .code(400)
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send({ err });
+  }
+});
+
+server.post("/erc721/transfer", async (request, reply) => {
+  try {
+    const response = await client.transferERC721(request.body);
+    reply
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send({ payload: response });
+  } catch (err) {
+    console.log("err", err);
+    reply
+      .code(400)
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send({ err });
+  }
+});
+
+server.post("/erc20/approve", async (request, reply) => {
+  try {
+    const response = await client.approveERC20(request.body);
+    reply
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send({ payload: response });
+  } catch (err) {
+    reply
+      .code(400)
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send({ err });
+  }
+});
+
+server.post("/erc721/approve", async (request, reply) => {
+  try {
+    const response = await client.approveERC721(request.body);
+    reply
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send({ payload: response });
+  } catch (err) {
+    reply
+      .code(400)
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send({ err });
+  }
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await server.listen(4000);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+start();
